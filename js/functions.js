54c1cbbefce8b6c54fc178629dc983e07ad5de72
Saz3r <easyer.tdd@gmail.com>
2024-02-14 16:20:22 +0600

    Создал первую функцию

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,5 @@
-
+function checkStringLength(string, maxLength) {
+  return string.length <= maxLength;
+}
+// eslint-disable-next-line no-console
+console.log(checkStringLength('Там бля то бля о бля уо бля вуэдаыжа',10));
