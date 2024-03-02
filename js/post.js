56daac124180d9c53171837c2a131cdc8606c290
Saz3r <easyer.tdd@gmail.com>
2024-03-02 20:06:44 +0600

    123

--- js/post.js
+++ js/post.js
@@ -1,36 +1,27 @@
 import{getRandomNumber,generateRandomComment} from './util.js'
 
-  // Функция для создания массива объектов-фотографий
-  export default function generatePhotosArray() {
-    const photos = [];
-    const usedIds = [];
-    const usedUrls = [];
-  
-    for (let i = 1; i <= 25; i++) {
-      const id = getRandomNumber(1, 1000);
-      while (usedIds.includes(id)) {
-        id = getRandomNumber(1, 1000);
-      }
-      usedIds.push(id);
-  
-      const url = 'photos/' + i + '.jpg';
-      while (usedUrls.includes(url)) {
-        url = 'photos/' + i + '.jpg';
-      }
-      usedUrls.push(url);
-  
-      const description = `'Описание фото'${i}`;
-      const likes = getRandomNumber(15, 200);
-  
-      const comments = [];
-      const numComments = getRandomNumber(0, 30);
-      for (let j = 0; j < numComments; j++) {
-        comments.push(generateRandomComment());
-      }
-  
-      photos.push({ id, url, description, likes, comments });
-    }
-  
-    return photos;
-  }
+const commentMin = 0;
+const commentMax = 30;
+const urlMin = 1;
+const urlMax = 25;
+const urlIndex = getRandomNumber(urlMin, urlMax);
+const url = `photos/${urlIndex}.jpg`;
+const commentsNum = getRandomNumber(commentMin, commentMax);
+
+// Функция для создания объекта фото
+const generatePhoto = function () {
+    const photo = {};
+    photo.id = getRandomNumber(1,25);
+    photo.url = url;
+    photo.description = 'Описание фото';
+    photo.likes = getRandomNumber(15, 200);
+    photo.comments = Array.from({ length: commentsNum }, generateRandomComment());
+    return photo;
+}
+
+// Функция для создания массива из 25 объектов photo
+const generatePhotosArray = function generatePhotosArray() {
+    const photosNum = 25;
+    return Array.from({ length: photosNum }, generatePhoto);
+}
   export{generatePhotosArray}
--- js/util.js
+++ js/util.js
@@ -1,26 +1,51 @@
-// Функция для генерации случайного числа от min до max
-function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
 
-    // Функция для генерации случайного комментария
-    function generateRandomComment() {
-        const avatars = ['img/avatar-1.svg', 'img/avatar-2.svg', 'img/avatar-3.svg', 'img/avatar-4.svg', 'img/avatar-5.svg', 'img/avatar-6.svg'];
-        const messages = [
-          'Всё отлично!',
-          'В целом всё неплохо. Но не всё.',
-          'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-          'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-          'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-          'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-        ];
-      
-        return {
-          id: getRandomNumber(1, 10000),
-          avatar: avatars[getRandomNumber(1, 6)],
-          message: messages[getRandomNumber(1, 6)],
-          name: 'User' + '-' + getRandomNumber(1, 100)
-        };
-      }
+// Получаем рандом число
+const getRandomNumber = function (a, b) {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  let previousNumber = null;
+  let randomNumber;
+  do {
+    randomNumber = Math.floor(Math.random() * (upper - lower + 1) + lower);
+  } while (randomNumber === previousNumber);
+  previousNumber = randomNumber;
+
+  return randomNumber;
+
+}
+
+
+// Функция для генерации случайного комментария
+const generateRandomComment =  function ()  {
+  const names = ["User-784923", "User-246837", "User-538492", "User-103857", "User-762948", "User-384729", "User-203847", "User-594024", "User-364920", "User-857364"];
+  const messages = [
+      'Всё отлично!',
+      'В целом всё неплохо. Но не всё.',
+      'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+      'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+      'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+      'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  ];
+  const avatarMin = 0;
+  const avatarMax = 5;
+  let id = 1;
+  const getMessageIndex = getRandomNumber(0, messages.length - 1);
+  const getNamesIndex = getRandomNumber(0, names.length - 1);
+  const avatarId = getRandomNumber(avatarMin, avatarMax);
+  return () => {
+      const comment = {};
+      const avatarId = getRandomNumber(avatarMin, avatarMax);
+      comment.id = getRandomNumber(1,150);
+      comment.avatar = `img/avatar-${avatarId}.svg`;
+      comment.message = messages[getMessageIndex];
+      comment.name = names[getNamesIndex];
+      id++;
+      return comment;
+  };
+}
+
+
+
+
   
 export{getRandomNumber,generateRandomComment}
