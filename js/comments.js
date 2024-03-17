b93b177e942954daf66a5f8c5b53d408b1858d32
Saz3r <easyer.tdd@gmail.com>
2024-03-18 01:51:57 +0500

    8.2

---
+++ js/comments.js
@@ -0,0 +1,50 @@
+let commentsArr = [];
+let currentComments = 5;
+const CommentsStep = 5;
+const socialComments = document.querySelector('.social__comments');
+const commentsCount = document.querySelector('.social__comment-count');
+const commentsLoader = document.querySelector('.comments-loader');
+socialComments.innerHTML = '';
+
+
+const loadAllComments = () => {
+
+  const socialCommentFragment = document.createDocumentFragment();
+  commentsCount.classList.remove('hidden');
+  const loadedComments = commentsArr.slice(currentComments, currentComments + CommentsStep);
+  //const loadedCommentsLength = loadedComments.length += CommentsStep;
+
+  loadedComments.forEach((comment) => {
+    const commentElement = document.createElement('li');
+    commentElement.classList.add('social__comment');
+    commentElement.innerHTML = `
+      <img class="social__picture" src="${comment.avatar}" alt="${comment.name}" width="35" height="35">
+      <p class="social__text">${comment.message}</p>
+    `;
+    socialComments.appendChild(commentElement);
+    socialComments.appendChild(socialCommentFragment);
+
+
+  });
+
+  commentsCount.querySelector('.social__comment-shown-count').textContent = currentComments >= commentsArr.length ? commentsArr.length : currentComments;
+  //commentsCount.querySelector('.social__comment-shown-count').textContent = loadedComments.length;
+  commentsCount.querySelector('.social__comment-total-count').textContent = commentsArr.length ;
+  if (currentComments >= commentsArr.length) {
+    commentsLoader.classList.add('hidden');
+  }
+  currentComments += CommentsStep;
+};
+const clear = () => {
+  socialComments.innerHTML = '';
+  commentsLoader.classList.remove('hidden');
+  currentComments = 0;
+  commentsLoader.removeEventListener('click', loadAllComments);
+
+};
+const loadComments = (comments) => {
+  commentsArr = comments;
+  loadAllComments();
+  commentsLoader.addEventListener('click', loadAllComments);
+};
+export{clear,loadComments};
