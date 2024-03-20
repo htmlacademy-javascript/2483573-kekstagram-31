9b25c4b62a5978a34e1d390c0502ccde2729bd0f
Saz3r <easyer.tdd@gmail.com>
2024-03-21 00:43:33 +0500

    to lower case

--- js/new-photos-forms-handler.js
+++ js/new-photos-forms-handler.js
@@ -13,7 +13,7 @@ const pristine = new Pristine(uploadForm, {
   classTo: 'img-upload__field-wrapper',
   errorTextParent:'img-upload__field-wrapper', // Элемент, на который будут добавляться классы
   errorTextClass: 'img-upload__field-wrapper--error', // Класс, обозначающий невалидное поле
-},false);
+});
 
 
 const hashTagsArrMaxLength = 5;
@@ -23,19 +23,23 @@ const validateHashTagsNumber = (value) => {
 };
 const validateHashTagsText = (value) => {
   const hashTagsArr = value.split(' ');
-  return hashTagsArr.every((element) => regex.test(element));
-
+  if(hashTagsInput.value !== '') {
+    return hashTagsArr.every((element) => regex.test(element));
+  }
+  return true;
 };
 const maxCommentLength = 140;
 const commentTextAreaValue = commentTextArea.value;
 const validateComments = () => commentTextAreaValue.length !== maxCommentLength;
 const validateUniqueHashTags = (value) => {
   const hashTagsArr = value.split(' ');
-  const dublicates = new Set(hashTagsArr).size !== hashTagsArr.length;
-  if(dublicates){
+  const lowerCaseHashTagsArr = hashTagsArr.map((tag) => tag.toLowerCase());
+  const duplicates = new Set(lowerCaseHashTagsArr).size !== lowerCaseHashTagsArr.length;
+  if (duplicates) {
     return false;
   }
-  return true;
+  return true; // Add this line to return true if no duplicates are found
+
 };
 
 pristine.addValidator(hashTagsInput,validateUniqueHashTags,'Хэштеги не должны повторяться ');
@@ -79,7 +83,10 @@ const openPhotoEditor = (evt) => {
 imgUploadInput.addEventListener('change', openPhotoEditor);
 
 
-uploadForm.addEventListener('submit',() => {
-
-  pristine.validate();
+uploadForm.addEventListener('submit',(event) => {
+  event.preventDefault();
+  const isValid = pristine.validate();
+  if(isValid){
+    uploadForm.submit();
+  }
 });
