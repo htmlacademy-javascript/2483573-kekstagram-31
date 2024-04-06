ce79fc572301e268654e7d0d1d49baa1d93855bb
Saz3r <easyer.tdd@gmail.com>
2024-04-06 22:16:36 +0500

    f

--- js/load-photos.js
+++ js/load-photos.js
@@ -1,27 +1,79 @@
-
 import{openBigPhoto} from './open-close-full-photo.js';
+import { comparePhotos } from './util.js';
 const picturesTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const dataErrorTemplate = document.querySelector('#data-error').content;
-
-
+const disscusedButton = document.querySelector('#filter-discussed');
+const randomButton = document.querySelector('#filter-random');
+const defaultButton = document.querySelector('#filter-default');
 const createdPhotosFragment = document.createDocumentFragment();
-
 const photosList = document.querySelector('.pictures');
+const imgFilter = document.querySelector('.img-filters'); // Use querySelector instead of querySelectorAll
+
+let discussedIsClicked = false;
+let randomIsClicked = false;
+let defaultIsClicked = true;
+
+const clickedFalse = () => {
+  discussedIsClicked = false;
+  randomIsClicked = false;
+  defaultIsClicked = false;
+};
+
+const setDefaultClick = () => {
+  clickedFalse();
+  defaultIsClicked = true;
+  imgFilter.classList.remove('img-filters__button--active');
+  defaultButton.classList.add('img-filters__button--active');
+
+};
+
+const setRandomClick = () => {
+  clickedFalse();
+  randomIsClicked = true;
+  imgFilter.classList.remove('img-filters__button--active');
+  randomButton.classList.add('img-filters__button--active');
+
+};
+
+const setDisscusedClick = () => {
+  clickedFalse();
+  discussedIsClicked = true;
+  imgFilter.classList.remove('img-filters__button--active');
+  disscusedButton.classList.add('img-filters__button--active');
+
+};
+
+disscusedButton.addEventListener('click', setDisscusedClick);
+defaultButton.addEventListener('click', setDefaultClick);
+randomButton.addEventListener('click', setRandomClick);
+
 const loadPhotos = (photos) => {
-  photos.forEach(({url, description, likes, comments, id}) => {
+  const sortedPhotos = photos.slice(); // Default to the original order
+  if (discussedIsClicked) {
+    sortedPhotos.sort(comparePhotos.disscused);
+  } if (randomIsClicked) {
+    sortedPhotos.sort(comparePhotos.random);
+  } if (defaultIsClicked){
+    sortedPhotos.slice();
+  }
+
+  photosList.innerHTML = ''; // Clear the photosList before appending
+  sortedPhotos.forEach(({ url, description, likes, comments, id }) => {
     const photosParts = picturesTemplate.cloneNode(true);
     photosParts.querySelector('img').src = url;
     photosParts.querySelector('img').alt = description;
     photosParts.querySelector('.picture__likes').textContent = likes;
     photosParts.querySelector('.picture__comments').textContent = comments.length;
     photosParts.dataset.id = id;
-    photosParts.addEventListener('click',(event) => {
-      const curentPicture = photos.find((photo) => event.currentTarget.dataset.id === photo.id.toString());
-      openBigPhoto(curentPicture);
+    photosParts.addEventListener('click', (event) => {
+      const currentPicture = photos.find((photo) => event.currentTarget.dataset.id === photo.id.toString());
+      openBigPhoto(currentPicture);
+      createdPhotosFragment.append(photosParts);
     });
-    createdPhotosFragment.append(photosParts);
+    // photosList.innerHTML = '';
+    photosList.append(createdPhotosFragment);
+
   });
-  photosList.append(createdPhotosFragment);
 };
 let dataErrorArea = dataErrorTemplate.cloneNode(true);
 
@@ -36,4 +88,4 @@ const showDataErrorMessage = () => {
 
 };
 
-export{loadPhotos,showDataErrorMessage};
+export{loadPhotos,showDataErrorMessage,setDefaultClick,setDisscusedClick,setRandomClick};
--- js/main.mjs
+++ js/main.mjs
@@ -1,17 +1,22 @@
 /* eslint-disable no-console */
-import { loadPhotos,showDataErrorMessage } from './load-photos.js';
+import { loadPhotos,setDefaultClick,showDataErrorMessage , setDisscusedClick, setRandomClick} from './load-photos.js';
 import { sendFormData , showSuccessWindow} from './new-photos-forms-handler.js';
 import { getData } from './api.js';
 import './load-photos.js';
 import './open-close-full-photo.js';
 import './new-photos-forms-handler.js';
 import './filter-redactor.js';
+const imgFilter = document.querySelectorAll('.img-filters');
 // import {generatePhotosArray} from './post.js';
 // const photosArray = generatePhotosArray();
 getData()
   .then((loadedPhotos) => {
     console.log(loadedPhotos);
     loadPhotos(loadedPhotos);
+    setDefaultClick(loadPhotos(loadedPhotos));
+    setRandomClick(loadPhotos(loadedPhotos));
+    setDisscusedClick(loadPhotos(loadedPhotos));
+    imgFilter.classList.remove('img-filters--inactive');
   })
   .catch(
     showDataErrorMessage
